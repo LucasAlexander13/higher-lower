aece93d71f58748e5d2daf4c1b3bede8aaa9983d
Lucas Alexander <lucasalexander13@hotmail.com>
2021-07-15 14:38:08 -0300

    Marvel movies dataset added

---
+++ dataset.py
@@ -0,0 +1,140 @@
+data = [
+    {
+        'name': 'Iron Man',
+        'company': 'Marvel',
+        'metascore': 79,
+        'release': 2008
+    },
+    {
+        'name': 'The Incredible Hulk',
+        'company': 'Marvel',
+        'metascore': 61,
+        'release': 2008
+    },
+    {
+        'name': 'Iron Man 2',
+        'company': 'Marvel',
+        'metascore': 57,
+        'release': 2010
+    },
+    {
+        'name': 'Thor',
+        'company': 'Marvel',
+        'metascore': 57,
+        'release': 2011
+    },
+    {
+        'name': 'Captain America: The First Avenger',
+        'company': 'Marvel',
+        'metascore': 66,
+        'release': 2011
+    },
+    {
+        'name': 'The Avengers',
+        'company': 'Marvel',
+        'metascore': 69,
+        'release': 2012
+    },
+    {
+        'name': 'Iron Man 3',
+        'company': 'Marvel',
+        'metascore': 62,
+        'release': 2013
+    },
+    {
+        'name': 'Thor: The Dark World',
+        'company': 'Marvel',
+        'metascore': 54,
+        'release': 2013
+    },
+    {
+        'name': 'Captain America: The Winter Soldier',
+        'company': 'Marvel',
+        'metascore': 70,
+        'release': 2014
+    },
+    {
+        'name': 'Guardians of the Galaxy',
+        'company': 'Marvel',
+        'metascore': 76,
+        'release': 2014
+    },
+    {
+        'name': 'Avengers: Age of Ultron',
+        'company': 'Marvel',
+        'metascore': 66,
+        'release': 2015
+    },
+    {
+        'name': 'Ant-Man',
+        'company': 'Marvel',
+        'metascore': 64,
+        'release': 2015
+    },
+    {
+        'name': 'Captain America: Civil War',
+        'company': 'Marvel',
+        'metascore': 75,
+        'release': 2016
+    },
+    {
+        'name': 'Doctor Strange',
+        'company': 'Marvel',
+        'metascore': 72,
+        'release': 2016
+    },
+    {
+        'name': 'Guardians of the Galaxy Vol. 2',
+        'company': 'Marvel',
+        'metascore': 67,
+        'release': 2017
+    },
+    {
+        'name': 'Spider-Man: Homecoming',
+        'company': 'Marvel',
+        'metascore': 73,
+        'release': 2017
+    },
+    {
+        'name': 'Thor: Ragnarok',
+        'company': 'Marvel',
+        'metascore': 74,
+        'release': 2017
+    },
+    {
+        'name': 'Black Panther',
+        'company': 'Marvel',
+        'metascore': 88,
+        'release': 2018
+    },
+    {
+        'name': 'Avengers: Infinity War',
+        'company': 'Marvel',
+        'metascore': 68,
+        'release': 2018
+    },
+    {
+        'name': 'Ant-Man and the Wasp',
+        'company': 'Marvel',
+        'metascore': 70,
+        'release': 2018
+    },
+    {
+        'name': 'Captain Marvel',
+        'company': 'Marvel',
+        'metascore': 64,
+        'release': 2019
+    },
+    {
+        'name': 'Avengers: Endgame',
+        'company': 'Marvel',
+        'metascore': 78,
+        'release': 2019
+    },
+    {
+        'name': 'Spider-Man: Far from Home',
+        'company': 'Marvel',
+        'metascore': 69,
+        'release': 2019
+    }
+]
